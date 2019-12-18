f04a84f64a592a67a6ec00f0395b57e0fe678496
GameMaster1928 <GameMaster1928@outlook.com>
2019-12-18 12:05:16 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
 var input = document.getElementById("select");
 
-function zelda() {
-    var resizeOwnEmulator = function(width, height) {
-        var emulator = $('#emulator');
-        emulator.css('width', width);
-        emulator.css('height', height);
-    }
-
-    $(function() {
-        function embed() {
-            var emulator = $('#emulator');
-            if(emulator) {
-                var flashvars = {
-                    system : 'nes',
-                    url : '/roms/Legend of Zelda.nes'
-                };
-                var params = {};
-                var attributes = {};
-
-                params.allowscriptaccess = 'sameDomain';
-                params.allowFullScreen = 'true';
-                params.allowFullScreenInteractive = 'true';
-
-                swfobject.embedSWF('flash/Nesbox.swf', 'emulator', '640', '480', '11.2.0', 'flash/expressInstall.swf', flashvars, params, attributes);
-            }
-        }
-
-    embed();
-    });
+var resizeOwnEmulator = function(width, height) {
+    var emulator = $('#emulator');
+    emulator.css('width', width);
+    emulator.css('height', height);
 }
 
-zelda();
+$(function() {
+    function embed() {
+        var emulator = $('#emulator');
+        if(emulator) {
+            var flashvars = {
+                system : 'nes',
+                url : '/roms/Legend of Zelda.nes'
+            };
+            var params = {};
+            var attributes = {};
+
+            params.allowscriptaccess = 'sameDomain';
+            params.allowFullScreen = 'true';
+            params.allowFullScreenInteractive = 'true';
+
+            swfobject.embedSWF('flash/Nesbox.swf', 'emulator', '640', '480', '11.2.0', 'flash/expressInstall.swf', flashvars, params, attributes);
+        }
+    }
+
+embed();
+});
