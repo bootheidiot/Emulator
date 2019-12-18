91e0b1fbbfbdf16ccc383ce0c2c279e07a1f2ecf
GameMaster1928 <GameMaster1928@outlook.com>
2019-12-18 12:40:00 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 var input = document.getElementById("select");
 
-var resizeOwnEmulator = function(width, height) {
-    var emulator = $('#emulator');
-    emulator.css('width', width);
-    emulator.css('height', height);
-}
-
-$(function() {
-    function embed() {
+function emulate(system, file) {
+    var resizeOwnEmulator = function(width, height) {
         var emulator = $('#emulator');
-        if(emulator) {
-            var flashvars = {
-                system : 'nes',
-                url : '/roms/Legend of Zelda.nes'
-            };
-            var params = {};
-            var attributes = {};
-
-            params.allowscriptaccess = 'sameDomain';
-            params.allowFullScreen = 'true';
-            params.allowFullScreenInteractive = 'true';
-
-            swfobject.embedSWF('flash/Nesbox.swf', 'emulator', window.innerWidth, window.innerHeight, '11.2.0', 'flash/expressInstall.swf', flashvars, params, attributes);
-        }
+        emulator.css('width', width);
+        emulator.css('height', height);
     }
 
-embed();
-});
+    $(function() {
+        function embed() {
+            var emulator = $('#emulator');
+            if(emulator) {
+                var flashvars = {
+                    system : system,
+                    url : '/roms/' + file
+                };
+                var params = {};
+                var attributes = {};
+
+                params.allowscriptaccess = 'sameDomain';
+                params.allowFullScreen = 'true';
+                params.allowFullScreenInteractive = 'true';
+
+                swfobject.embedSWF('flash/Nesbox.swf', 'emulator', window.innerWidth, window.innerHeight, '11.2.0', 'flash/expressInstall.swf', flashvars, params, attributes);
+                resizeOwnEmulator(window.innerWidth, window.innerHeight);
+            }
+        }
+
+    embed();
+    });
+}
+
+emulate("nes", "Legend of Zelda.nes");
